ad9da063d6914adcef04fe61549db400c4dd9e5d
Mark <markDrigola2802@gmail.com>
2017-01-31 00:56:07 +0200

    second

--- js/common.js
+++ js/common.js
@@ -22,5 +22,35 @@
             dots: true,
             arrows: true
         });
+
+        //Scroll head menu
+        var blockHeadMenu = $('.head-init-script');
+        var tempScrollTop, currentScrollTop = 0;
+        $(window).on('scroll', function () {
+            var thisScrollTop = $(this).scrollTop();
+
+            currentScrollTop = $(document).scrollTop();
+            if($(this).scrollTop() > blockHeadMenu.offset().top) {
+                blockHeadMenu.addClass('menu-fixed');
+                $('.menu-fixed').css('top','-62px');
+                blockHeadMenu.parent().css({
+                    'paddingTop': blockHeadMenu.height() + 'px',
+                })
+            }
+            if(tempScrollTop < currentScrollTop) {
+                $('.menu-fixed').css('top','-62px');
+            } else if (tempScrollTop > currentScrollTop ) {
+                $('.menu-fixed').css('top','0');
+            }
+
+            if($(this).scrollTop() === 0) {
+                blockHeadMenu.removeClass('menu-fixed');
+                blockHeadMenu.parent().css({
+                    'paddingTop': 0,
+                })
+            }
+
+            tempScrollTop = currentScrollTop;
+        });
     });
 })();
